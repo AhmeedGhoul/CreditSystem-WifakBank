77094badd84fe56da1fb6e7ff8f6c3dd16b944ab
ahmed <ahmed.ghoul@esprit.tn>
2025-08-06 18:39:41 +0100

    normally 95% of the project

--- src/activity_log/activity_log.module.ts
+++ src/activity_log/activity_log.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ActivityLogService } from './activity_log.service';
 import { AuditModule } from './audit/audit.module';
-import { FraudCaseModule } from './fraud_case/fraud_case.module';
 
 @Module({
-  imports: [AuditModule, FraudCaseModule],
+  imports: [AuditModule],
   providers: [ActivityLogService],
   exports: [ActivityLogService],
 })
--- src/activity_log/fraud_case/fraud_case.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FraudCaseService } from './fraud_case.service';
-
-@Module({
-  providers: [FraudCaseService]
-})
-export class FraudCaseModule {}
